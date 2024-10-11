5d87a7885393b226b62e1edba025a3e6bb6fac02
Eganathan R <ammasamrith@gmail.com>
2024-10-11 23:47:02 +0530

    Create supportedmarkdowntokens.js

---
+++ supportedmarkdowntokens.js
@@ -0,0 +1,30 @@
+{
+    "bold": {
+        "token": "*",
+        "tag": "strong"
+    },
+    "italic": {
+        "token": "_",
+        "tag": "em"
+    },
+    "underline": {
+        "token": "__",
+        "tag": "u"
+    },
+    "strikethrough": {
+        "token": "~",
+        "tag": "del"
+    },
+    "unorderedList": {
+        "token": "-",
+        "tag": "ul"
+    },
+    "inlineCode": {
+        "token": "```",
+        "tag": "code"
+    },
+    "blockQuote": {
+        "token": "|",
+        "tag": "blockquote"
+    }
+}
